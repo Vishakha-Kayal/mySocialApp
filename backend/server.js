b3c7ce1db8f2ba92d7bfd210f8d43eb0f331fdd6
Vishakha-Kayal <138288780+Vishakha-Kayal@users.noreply.github.com>
2024-06-28 17:18:11 +0530

    setting for production

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(cors()); 
 app.use('/user', userRoutes);
 app.use('/api', postRoutes);
 
-
-app.get('/', (req,res) => {
-    res.send("good to go");
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
