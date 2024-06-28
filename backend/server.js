fea7a81c7617277675a6f00d51fa8b1382784413
Vishakha-Kayal <138288780+Vishakha-Kayal@users.noreply.github.com>
2024-06-28 17:32:34 +0530

    ready for production

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,16 @@ const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
 
 const app = express();
-app.use(express.static(path.join(__dirname, '..', 'frontend','build')));
 app.use(bodyParser.json());
 app.use(cors()); 
+
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'my-mern-project', 'dist')));
+
 app.use('/user', userRoutes);
 app.use('/api', postRoutes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'my-mern-project', 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
